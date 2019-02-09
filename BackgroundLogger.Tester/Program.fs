5330acb4404f4379bbf3f19fe47222168e7b3c18
Michal Duda <michal.duda@ncr.com>
2019-02-09 20:04:13 +0100

    Initial commit

---
+++ BackgroundLogger.Tester/Program.fs
@@ -0,0 +1,26 @@
+﻿// Learn more about F# at http://fsharp.org
+
+open System
+open BackgroundLogger.Core
+open System.Threading
+open System.Reactive.Linq
+open System.Reactive.Linq
+
+let slowAppender = {new IAppender with
+                        member this.Log(messages: Collections.Generic.IList<string>): unit = 
+                            Thread.Sleep(1000)
+                            for m in messages do Console.WriteLine(m)
+                            }
+
+[<EntryPoint>]
+let main argv =
+    
+    let logger = createBackgroundLogger [slowAppender] (TimeSpan.FromSeconds(1.0)) 5 30
+
+    for i = 1 to 100 do
+        logger.Log(sprintf "Test %i" i)
+
+    Console.ReadKey()
+
+    printfn "Hello World from F#!"
+    0 // return an integer exit code
---
+++ README.md
@@ -0,0 +1,3 @@
+# BackgroundLogger
+
+Experimental library for logging on background thread (useful for example when logs are being pushed to ElasticSearch). Implemented on top of Rx.NET library.
